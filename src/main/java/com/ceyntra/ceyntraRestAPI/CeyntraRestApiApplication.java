2246c0e942f4ac3a1369b5878b6a458f03b3f034
shehan82 <shehansandeepa82@gmail.com>
2021-07-08 21:21:45 +0530

    project initialized

---
+++ src/main/java/com/ceyntra/ceyntraRestAPI/CeyntraRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ceyntra.ceyntraRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeyntraRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeyntraRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceyntra/ceyntraRestAPI/CeyntraRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceyntra.ceyntraRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeyntraRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
