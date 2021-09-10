a4917772bc5fe1960537bc0b9a176fd06ec37d68
Isuru-Diyawinna <isuruprasad110@gmail.com>
2021-09-10 17:04:45 +0530

    done getPackage mapping

--- src/main/java/com/ceyntra/ceyntraRestAPI/controller/TaxiPackageController.java
+++ src/main/java/com/ceyntra/ceyntraRestAPI/controller/TaxiPackageController.java
@@ -3,9 +3,9 @@ package com.ceyntra.ceyntraRestAPI.controller;
 import com.ceyntra.ceyntraRestAPI.entity.TaxiPackageEntity;
 import com.ceyntra.ceyntraRestAPI.repository.TaxiPackageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TaxiPackageController {
@@ -20,6 +20,10 @@ public class TaxiPackageController {
         return taxiPackageRepository.save(taxiPackage);
     }
 
-
+    @GetMapping("getPackageDetails/{id}")
+    public List<TaxiPackageEntity> getPackageDetails(@PathVariable int id){
+        List<TaxiPackageEntity> packageList=taxiPackageRepository.getAllPackages(id);
+        return packageList;
+    }
 
 }
