1a51b7733a29dc48168b8755fad75a0dbc6f5050
Isuru-Diyawinna <isuruprasad110@gmail.com>
2021-09-10 17:07:12 +0530

    done getPackages query

--- src/main/java/com/ceyntra/ceyntraRestAPI/repository/TaxiPackageRepository.java
+++ src/main/java/com/ceyntra/ceyntraRestAPI/repository/TaxiPackageRepository.java
@@ -2,10 +2,15 @@ package com.ceyntra.ceyntraRestAPI.repository;
 
 import com.ceyntra.ceyntraRestAPI.entity.TaxiPackageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TaxiPackageRepository extends JpaRepository<TaxiPackageEntity,Integer> {
 
-
+    @Query("SELECT a FROM TaxiPackageEntity a WHERE a.taxiDriverId= :id")
+    public List<TaxiPackageEntity> getAllPackages(@Param("id") int id);
 }
