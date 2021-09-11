f6178d96d99e4cbf60ba1be1b4da8f4f4f195373
shashiradw <shashiradw7@gmail.com>
2021-09-11 22:58:40 +0530

    Hotel packages add & display done

--- src/main/java/com/ceyntra/ceyntraRestAPI/controller/TaxiPackageController.java
+++ src/main/java/com/ceyntra/ceyntraRestAPI/controller/TaxiPackageController.java
@@ -3,9 +3,10 @@ package com.ceyntra.ceyntraRestAPI.controller;
 import com.ceyntra.ceyntraRestAPI.entity.TaxiPackageEntity;
 import com.ceyntra.ceyntraRestAPI.repository.TaxiPackageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TaxiPackageController {
@@ -19,6 +20,15 @@ public class TaxiPackageController {
 
         return taxiPackageRepository.save(taxiPackage);
     }
+
+    @GetMapping("/taxiPackages/{taxiDriverId}")
+    public List<TaxiPackageEntity> getAllTaxiPackagesByTaxiID(@PathVariable("taxiDriverId") int taxiDriverId){
+
+        return taxiPackageRepository.getTaxiPackageEntitiesByTaxiDriverId(taxiDriverId);
+
+    }
+
+
 
 
 
--- src/main/java/com/ceyntra/ceyntraRestAPI/repository/TaxiPackageRepository.java
+++ src/main/java/com/ceyntra/ceyntraRestAPI/repository/TaxiPackageRepository.java
@@ -4,8 +4,11 @@ import com.ceyntra.ceyntraRestAPI.entity.TaxiPackageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaxiPackageRepository extends JpaRepository<TaxiPackageEntity,Integer> {
 
+    List<TaxiPackageEntity> getTaxiPackageEntitiesByTaxiDriverId(int taxiDriverId);
 
 }
