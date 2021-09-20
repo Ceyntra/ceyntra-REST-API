2e4e0cab88f209a9ee2ee9f97593bcb0bea6668a
Isuru-Diyawinna <isuruprasad110@gmail.com>
2021-09-20 19:21:40 +0530

    guide package update and delete done

--- src/main/java/com/ceyntra/ceyntraRestAPI/controller/GuidePackageController.java
+++ src/main/java/com/ceyntra/ceyntraRestAPI/controller/GuidePackageController.java
@@ -2,6 +2,7 @@ package com.ceyntra.ceyntraRestAPI.controller;
 
 import com.ceyntra.ceyntraRestAPI.entity.GuidePackageEntity;
 import com.ceyntra.ceyntraRestAPI.entity.HotelPackageEntity;
+import com.ceyntra.ceyntraRestAPI.entity.TaxiPackageEntity;
 import com.ceyntra.ceyntraRestAPI.repository.GuidePackageRepository;
 import com.ceyntra.ceyntraRestAPI.repository.HotelPackageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +25,16 @@ public class GuidePackageController {
     public GuidePackageEntity createTaxiPackage(@RequestBody GuidePackageEntity guidePackage){
         System.out.println(guidePackage.toString());
         return guidePackageRepository.save(guidePackage);
+    }
+
+    @PutMapping("/updateGuidePackage")
+    public void updateTaxiPackage(@RequestBody GuidePackageEntity guidePackage){
+
+        guidePackageRepository.save(guidePackage);
+    }
+    @DeleteMapping("deleteGuidePackage/{id}")
+    public void deleteGuidePackage(@PathVariable int id){
+
+        guidePackageRepository.deleteById(id);
     }
 }
